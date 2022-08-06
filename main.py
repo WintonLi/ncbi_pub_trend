834c258c5442ac91b6c2c9200ce4093acbf468bf
winton <yunxili@axiomamc.com>
2022-08-06 22:58:09 +0100

    fix cros

--- main.py
+++ main.py
@@ -4,16 +4,44 @@
 # @Author  : liyun
 # @desc    :
 import uvicorn
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, status
 from typing import List
 from ncbi_app.schemas import NumPubOfYear
 from ncbi_app.ncbi_api import download_trend
 from settings import settings
+from starlette.requests import Request
+from fastapi.responses import JSONResponse
+from starlette.middleware.errors import ServerErrorMiddleware
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.types import ASGIApp
 
 
 app = FastAPI(title="NCBI API FastAPI Application",
               description="FastAPI Application that discovers the trend of NCBI publications",
               version="1.0.0", )
+
+
+# TODO: remove the following usage when the CROS bug is fixed
+async def global_execution_handler(request: Request, exc: Exception) -> ASGIApp:
+    return JSONResponse(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        content="Unknown Error",
+    )
+app.add_middleware(
+    ServerErrorMiddleware,
+    handler=global_execution_handler,
+)
+
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get('/trend', response_model=List[NumPubOfYear])
